337f04e194c1099d1f00609678f0d0654c6341f8
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-04-16 17:33:12 +0100

    create Track component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Track from ;
+// import Track from '../Track/Track';
 import './TrackList.css';
 
 export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* You will add a map method that renders a set of Track components */}
+        
       </div>
     );
   }
