48bc6e5a1860f6bb3abc667b43997cae02965202
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-04-16 14:12:32 +0100

    create TrackList component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import TrackList from ;
 import './Playlist.css';
 
-export class Playlist extends React.Component {
+class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
@@ -13,3 +13,5 @@ export class Playlist extends React.Component {
     );
   }
 }
+
+export default Playlist;
